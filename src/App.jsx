7248d94b3e54bec539e2d11d00b1ad964b27ec5f
chrisHebs23 <christopher.heberden@gmail.com>
2023-08-22 17:57:53 -0400

    Fixed Scroll problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Layout from "./Layout/Layout";
+import ScrollToTop from "./hooks/ScrollToTop";
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
+        <ScrollToTop />
         <Layout />
       </BrowserRouter>
     </React.StrictMode>
